ac237178530031e0419fa3bd86eb6228f6cb58c4
Cristian Zambrano <cristian_uteq@hotmail.com>
2025-12-27 16:10:35 -0500

    Changes on Config3

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/tienda';
+    //public string $baseURL = 'http://localhost/tienda';
+    public string $baseURL = 'http://appwebuteq.us-east-1.elasticbeanstalk.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
